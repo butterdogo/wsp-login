f88ff87e84f13b096c349200222c2f8d1e90f2c2
olle <olle.axelsson@elev.ga.ntig.se>
2025-03-11 10:30:47 +0100

    .

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 import "dotenv/config"
 import express from "express"
 import nunjucks from "nunjucks"
+import morgan from "morgan"
+import logger from "morgan"
+import bcrypt from "bcrypt"
+import session from "express-session"
+
+
+
+
 
 const app = express()
 const port = 3000
@@ -10,6 +18,15 @@ nunjucks.configure("views", {
   express: app,
 })
 
+app.use(logger("dev"))
+
+app.use(session({
+  secret: "keyboard cat",
+  resave: false,
+  saveUninitialized: true,
+  cookie: { sameSite: true }
+}))
+
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
@@ -17,6 +34,15 @@ app.get("/", (req, res) => {
     { title: "Qvixter", message: "Best service, legit." }
   )
 })
+
+let myPlaintextPassword = "detlösenordsomduvillha"
+bcrypt.hash(myPlaintextPassword, 10, function(err, hash) {
+	// här får vi nu tag i lösenordets hash i variabeln hash
+	console.log(hash)
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
